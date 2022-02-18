4cc05ee3770b064405148872f614062dce130b68
Boris <malikovicboris85@gmail.com>
2022-02-18 23:59:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 "use strict";
 
-document.querySelector(".add").addEventListener("click", function () {});
+const input = document.querySelector(".input");
+const list = document.querySelector(".list");
+const add = document.querySelector(".add");
+const item = document.querySelector("li");
+
+const addItem = function () {
+  const item = document.createElement("li");
+  const deleteItem = document.createElement("button");
+  deleteItem.textContent = "Delete";
+  if (input.value.length > 0) {
+    item.textContent = input.value;
+    list.appendChild(item);
+    list.appendChild(deleteItem);
+    input.value = "";
+  }
+  // Add class done
+  item.addEventListener("click", function () {
+    item.classList.toggle("done");
+  });
+};
+
+// Add item
+add.addEventListener("click", addItem);
+
+// Add item with Enter
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addItem();
+  }
+});
